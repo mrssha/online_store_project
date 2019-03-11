04b4d949a728772a46d460ac663a72f885436b4c
mrssha <shalnova.mary@gmail.com>
2019-03-11 13:46:51 +0300

    + Entity Oder

--- src/main/java/dbservice/entity/Order.java
+++ src/main/java/dbservice/entity/Order.java
@@ -1,4 +1,245 @@
 package dbservice.entity;
 
+import javax.persistence.*;
+import java.util.*;
+
+
+@Entity
+@Table(name = "order")
 public class Order {
+
+    @Id
+    @Column(name = "id_order")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_customer", referencedColumnName = "id_customer")
+    private Customer customer;
+
+
+    //@Column(name = "id_customer_address")
+    //private Long idCustomerAddress;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_customer_address")
+    private Address customerAddress;
+    // Связь односторонняя
+    // Нужно ли вообще делать эту связь?
+
+    @Column(name = "date_order", nullable = false)
+    private Date dateOrder;
+
+    @Column(name = "quantity_products")
+    private Integer quantityProducts;
+
+    @Column(name = "payment_amount")
+    private Double payment_amount;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_method", columnDefinition = "CARD")
+    private PaymentMethod paymentMethod;
+
+    public static enum PaymentMethod{
+        CASH,
+        CARD
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "delivery_method", columnDefinition = "SELF_DELIVERY")
+    private DeliveryMethod deliveryMethod;
+
+    public static enum DeliveryMethod{
+        COURIER,
+        POSTAMPT,
+        SELF_DELIVERY
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status", columnDefinition = "WAITING")
+    private PaymentStatus paymentStatus;
+
+
+    public static enum PaymentStatus{
+        WAITING,
+        PAID
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status", columnDefinition = "WAIT_PAYMENT")
+    private OrderStatus orderStatus;
+
+    public static enum OrderStatus{
+        WAIT_PAYMENT,
+        WAIT_SHIPMENT,
+        SHIPPED,
+        DELIVERED
+    }
+
+
+    /*
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns ({
+        @JoinColumn(name = "id_customer", referencedColumnName = "id_customer"),
+        @JoinColumn(name = "id_customer_address",referencedColumnName = "id_address")
+    })
+    private Customer customer;
+    */
+
+    /*
+    // Разделяем на OneToMany + ManyToOne
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "Basket",
+            joinColumns =@JoinColumn(name = "id_order", referencedColumnName = "id_order"),
+            inverseJoinColumns = @JoinColumn(name = "id_product", referencedColumnName = "id_product")
+    )
+    private Set<Product> products;
+    */
+
+
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Basket> products = new ArrayList<>();
+    //private List<Basket> products = new ArrayList<>();
+    // Нужно инициализировать?
+
+
+    public Order(){
+    }
+
+
+    public void addProduct(Product product) {
+        Basket orderProduct = new Basket(this, product);
+        products.add(orderProduct);
+        product.getOrders().add(orderProduct);
+    }
+
+    public void removeProduct(Product product) {
+        Iterator<Basket> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Basket orderProduct = iterator.next();
+
+            if (orderProduct.getOrder().equals(this) &&
+                    orderProduct.getProduct().equals(product)) {
+                iterator.remove();
+                orderProduct.getProduct().getOrders().remove(orderProduct);
+                orderProduct.setOrder(null);
+                orderProduct.setProduct(null);
+            }
+        }
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Address getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(Address customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public Date getDateOrder() {
+        return dateOrder;
+    }
+
+    public void setDateOrder(Date dateOrder) {
+        this.dateOrder = dateOrder;
+    }
+
+    public Integer getQuantityProducts() {
+        return quantityProducts;
+    }
+
+    public void setQuantityProducts(Integer quantityProducts) {
+        this.quantityProducts = quantityProducts;
+    }
+
+    public Double getPayment_amount() {
+        return payment_amount;
+    }
+
+    public void setPayment_amount(Double payment_amount) {
+        this.payment_amount = payment_amount;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public DeliveryMethod getDeliveryMethod() {
+        return deliveryMethod;
+    }
+
+    public void setDeliveryMethod(DeliveryMethod deliveryMethod) {
+        this.deliveryMethod = deliveryMethod;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+
+    public List<Basket> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Basket> products) {
+        this.products = products;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj == null || obj.getClass()!= Order.class){
+            return false;
+        }
+        Order order = (Order)obj;
+        return order.getId().equals(this.getId());
+    }
+
+
+
 }
